79f7cc60043b04f81c122f2fe318bf9a146632ed
Catalin Rusu <printul@gmail.com>
2017-05-23 15:08:04 +0100

    added views and finished controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
@@ -8,18 +8,35 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
+    redirect_to tasks_path
   end
 
   def create
+    Task.create(task_params)
+    redirect_to tasks_path
   end
 
   def edit
   end
 
   def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
   end
 
   def destroy
+    @task.delete
+    redirect_to tasks_path
   end
 
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:name, :details, :completed)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.create(name: "sleep", details: "sleep as much as possible", completed: false)
+Task.create(name: "eat", details: "eat as much as possible", completed: true)
+Task.create(name: "code", details: "code as little as possible", completed: true)
